256e8cb2beb7ddd3a6d264a58bc9788a89115286
Loan Tran <loan.tran@katalon.com>
2022-11-01 12:00:43 +0700

    test

--- Scripts/New Test Case/Script1667276452303.groovy
+++ Scripts/New Test Case/Script1667276452303.groovy
@@ -17,9 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('https://webapp-attack-2.azurewebsites.net')
 
-WebUI.navigateToUrl('https://webapp-attack-2.azurewebsites.net/')
+//WebUI.navigateToUrl('https://webapp-attack-2.azurewebsites.net/')
 
 WebUI.click(findTestObject('Object Repository/Page_Welcome/a_Sign In'))
 
