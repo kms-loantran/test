b705a5e396f45a475e8c82fc2cf858d860e25e71
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-02-02 18:19:54 +0700

    test

--- Test Suites/New Test Suite Collection.ts
+++ Test Suites/New Test Suite Collection.ts
@@ -4,7 +4,7 @@
    <name>New Test Suite Collection</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
-   <executionMode>SEQUENTIAL</executionMode>
+   <executionMode>PARALLEL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
