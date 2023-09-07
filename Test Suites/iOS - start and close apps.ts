b756ce6d74e68fcac1ecad7e0250b3a5bd3c1bcf
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-09-07 18:03:20 +0700

    test

---
+++ Test Suites/iOS - start and close apps.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>iOS - start and close apps</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>3</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1de3dc59-1255-4678-98f1-127162f3034d</testSuiteGuid>
+   <testCaseLink>
+      <guid>02015ed7-5cb3-4853-8012-c47fe1d9fd99</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/iOS - start and close apps</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
