c3a326a357c03f0c2f5ea9cd146b8a7fa455eaae
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-09-08 16:50:09 +0700

    test

--- Test Suites/New Test Suite Collection - test email.ts
+++ Test Suites/New Test Suite Collection - test email.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>New Test Suite Collection</name>
+   <name>New Test Suite Collection - test email</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
