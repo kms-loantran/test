bf8f3340e7343e0d756c7f0f71961b451cfa1f06
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-10-20 14:36:59 +0700

    test

--- Test Suites/DriverFactory_getExecutedBrowser.ts
+++ Test Suites/DriverFactory_getExecutedBrowser.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>DriverFactory.getExecutedBrowser() and DriverFactory.getExecutedBrowser().getName()</name>
+   <name>DriverFactory_getExecutedBrowser</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
