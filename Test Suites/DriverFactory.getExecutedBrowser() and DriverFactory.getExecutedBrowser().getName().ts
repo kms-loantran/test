c856c44bd9d01bfbf9746c981a497a09c24cd3f4
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-10-20 14:29:08 +0700

    test

---
+++ Test Suites/DriverFactory.getExecutedBrowser() and DriverFactory.getExecutedBrowser().getName().ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DriverFactory.getExecutedBrowser() and DriverFactory.getExecutedBrowser().getName()</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>3</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>efe043ed-ce40-45b9-9295-9ba0d32edb46</testSuiteGuid>
+   <testCaseLink>
+      <guid>9323feeb-445e-4606-ac44-ec1f0e9ecb8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DriverFactory.getExecutedBrowser() and DriverFactory.getExecutedBrowser().getName()</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
