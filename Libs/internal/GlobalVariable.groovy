65a2453925533104090d0f05e26af1bd1e306eb2
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-10-31 15:45:42 +0700

    test

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -24,7 +24,7 @@ public class GlobalVariable {
         try {
             def selectedVariables = TestCaseMain.getGlobalVariables("default")
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
-            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
     
             username = selectedVariables['username']
             pass = selectedVariables['pass']
